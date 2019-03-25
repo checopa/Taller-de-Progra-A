78a722766aa739cfe9d86db7989a7327fc757cfa
José Pacheco <43217967+checopa@users.noreply.github.com>
2019-03-25 18:56:31 -0300

    Update chloe.cpp

--- Tarea 1/chloe.cpp
+++ Tarea 1/chloe.cpp
@@ -1,13 +1,19 @@
 #include "iostream"
 #include "vector"
 #include "string"
+#include <sstream>
 using namespace std;
 
+string itos(int num){
+  stringstream s;
+  s<<num;
+  return s.str();
+}
 string secuencia(int n){
 	vector<string> vec;
 	vec.push_back("1");
 	for(int i=0;i<(n-1);i++){
-		string valor=to_string(i+2);
+		string valor=itos(i+2);
 		vec.push_back(vec[i]+valor+vec[i]);
 	}
 	return vec[vec.size()-1];
